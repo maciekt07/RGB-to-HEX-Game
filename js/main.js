55e27479962322f49bda28b2d508ae5bb1e76cbf
maciekkoks <85953204+maciekkoks@users.noreply.github.com>
2021-11-26 20:54:28 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var points = 0
-github = "https://github.com/maciekkoks/"
+const github = "https://github.com/maciekkoks/"
 
 function main() {
     document.getElementById("submit").style.display = "block"
